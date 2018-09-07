3c90817efa18496bc0c05ece92e56b723f6d2012
sherazmalik06 <sherazmalik06@gmail.com>
2018-09-07 13:43:54 +0500

    upload A

--- b/b.php
+++ b/b.php
@@ -11,11 +11,11 @@
 add_action( 'init', 'activate' );
 function activate()
 {
-	require_once ( 'wp_autoupdate.php' );
+	//require_once ( 'wp_autoupdate.php' );
 	$plugin_current_version = '1.0';
 	$plugin_remote_path = 'https://raw.githubusercontent.com/sherazmalik06/update-plugin/master/plugin.json';
 	$plugin_slug = plugin_basename( __FILE__ );
 	$license_user = 'user';
 	$license_key = 'abcd';
-	new WP_AutoUpdate ( $plugin_current_version, $plugin_remote_path, $plugin_slug, $license_user, $license_key );
+	//new WP_AutoUpdate ( $plugin_current_version, $plugin_remote_path, $plugin_slug, $license_user, $license_key );
 }
