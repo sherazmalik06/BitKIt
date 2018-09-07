cdbbbd5a3ddbd584f085dfc59a0f3ab61fdd7685
sherazmalik06 <sherazmalik06@gmail.com>
2018-09-07 12:46:59 +0500

    version updated

--- a/a.php
+++ a/a.php
@@ -4,7 +4,7 @@
  Plugin URI: http://bitbytes.io
  Description: Plugin with auto-updates from external URL/Repository using Github
  Author: BitBytes
- Version: 1.0
+ Version: 1.1
  */
 
 // Load the auto-update class
@@ -12,7 +12,7 @@ add_action( 'init', 'a_activate' );
 function a_activate()
 {
 	require_once ( 'a_update.php' );
-	$plugin_current_version = '1.0';
+	$plugin_current_version = '1.1';
 	$plugin_remote_path = 'https://github.com/sherazmalik06/BitKIt/blob/master/plugins.json';
 	$plugin_slug = plugin_basename( __FILE__ );
 	new a_update ( $plugin_current_version, $plugin_remote_path, $plugin_slug );
