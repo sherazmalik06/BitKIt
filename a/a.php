027dca999c4a1fd29212d85ab66b26bc8599b9db
sherazmalik06 <sherazmalik06@gmail.com>
2018-09-07 14:59:20 +0500

    Correct Data

--- a/a.php
+++ a/a.php
@@ -6,16 +6,3 @@
  Author: BitBytes
  Version: 1.0
  */
-
-// Load the auto-update class
-add_action( 'init', 'a_activate' );
-function a_activate()
-{
-	
-	$plugin_current_version = '1.0';
-	$plugin_remote_path = 'https://github.com/sherazmalik06/BitKIt/blob/master/plugins.json';
-	$plugin_slug = plugin_basename( __FILE__ );
-	$license_user = 'user';
-	$license_key = 'abcd';
-	//new WP_AutoUpdate ( $plugin_current_version, $plugin_remote_path, $plugin_slug, $license_user, $license_key );
-}
